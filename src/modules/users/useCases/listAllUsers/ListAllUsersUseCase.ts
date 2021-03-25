8bb67cbe21f3246e057b5414cd222791e4b65763
Felipe de Almeida <felipeapellegrini@gmail.com>
2021-03-25 01:19:18 -0300

    feat: implement useCases CreateUser and ListAllUsers

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,11 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userAdmin = this.usersRepository.findById(user_id);
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
