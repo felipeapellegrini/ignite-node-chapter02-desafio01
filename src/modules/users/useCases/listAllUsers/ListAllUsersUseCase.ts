5f84dbf4ab10b7be2c7166b9753640236dd1aca8
Felipe de Almeida <felipeapellegrini@gmail.com>
2021-03-25 08:58:22 -0300

    refactor: ensure unon existing or non admin user get users list

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    const userAdmin = this.usersRepository.findById(user_id);
+    const checkUserExists = this.usersRepository.findById(user_id);
+
+    if (!checkUserExists) {
+      throw new Error("User does not exist.");
+    }
+
+    if (!checkUserExists.admin) {
+      throw new Error("Admin privileges required");
+    }
 
     const users = this.usersRepository.list();
 
