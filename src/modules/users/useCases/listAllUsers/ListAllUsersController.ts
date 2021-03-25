ef62fa4b14eb50262bcbfbc5d4a8dc2bd7763dbe
Felipe de Almeida <felipeapellegrini@gmail.com>
2021-03-25 09:40:16 -0300

    refactor: ensure controller throws when useCase trhows

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,11 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // POG né.. fazer o que
-    const user_id = request.headers.user_id as string;
-    const users = this.listAllUsersUseCase.execute({ user_id });
+    try {
+      // POG né.. fazer o que
+      const user_id = request.headers.user_id as string;
+      const users = this.listAllUsersUseCase.execute({ user_id });
 
-    return response.json(users);
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
